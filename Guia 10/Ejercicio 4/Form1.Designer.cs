4587c7f036153b827b36dc04b6bee3d4221786b0
valentin-munoz03 <valentin.munoz03@gmail.com>
2026-06-12 01:41:18 -0300

    Ejercicio 4

---
+++ Guia 10/Ejercicio 4/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Ejercicio_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudHora = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudMinuto = new System.Windows.Forms.NumericUpDown();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbMinutos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHora)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinuto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tiempo en H:M";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nudHora
+            // 
+            this.nudHora.Location = new System.Drawing.Point(86, 47);
+            this.nudHora.Maximum = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            this.nudHora.Name = "nudHora";
+            this.nudHora.Size = new System.Drawing.Size(33, 20);
+            this.nudHora.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = ":";
+            // 
+            // nudMinuto
+            // 
+            this.nudMinuto.Location = new System.Drawing.Point(141, 47);
+            this.nudMinuto.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.nudMinuto.Name = "nudMinuto";
+            this.nudMinuto.Size = new System.Drawing.Size(33, 20);
+            this.nudMinuto.TabIndex = 3;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(189, 44);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertir.TabIndex = 4;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tiempo en minutos:";
+            // 
+            // lbMinutos
+            // 
+            this.lbMinutos.AutoSize = true;
+            this.lbMinutos.Location = new System.Drawing.Point(125, 94);
+            this.lbMinutos.Name = "lbMinutos";
+            this.lbMinutos.Size = new System.Drawing.Size(52, 13);
+            this.lbMinutos.TabIndex = 6;
+            this.lbMinutos.Text = "lbMinutos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 116);
+            this.Controls.Add(this.lbMinutos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.nudMinuto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nudHora);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudHora)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinuto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudHora;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudMinuto;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbMinutos;
+    }
+}
+
---
+++ Guia 10/Ejercicio 4/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            int hora = Convert.ToInt32(nudHora.Value);
+            int minuto = Convert.ToInt32(nudMinuto.Value);
+            int Hminuto = hora * 60;
+            int totalminutos = Hminuto + minuto;
+            lbMinutos.Text = totalminutos.ToString();          
+        }
+    }
+}
